c554bf64c81767be5d79840e7a49196799aa12b0
arsh <arshkasid046@gmail.com>
2023-01-20 01:21:21 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import ExportExcel from './excelexport';
 import EventTable from './compoment/reactTable';
 import ReactLandinng from './compoment/ReactLanding';
 import {Routes, Route} from 'react-router-dom';
+import { Navbar ,Nav} from 'react-bootstrap';
 
 
 function App() {
   return (
     <>
-    
+    <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">My App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 <Routes>
   <Route path="/" element={<ReactLandinng/>} />
   <Route path="/reactdata" element={<Reactdata/>} />
--- src/compoment/ReactLanding.js
+++ src/compoment/ReactLanding.js
@@ -4,17 +4,7 @@ import { Jumbotron, Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 function LandingPage() {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#">My Landing Page</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      
       <Jumbotron className="text-center">
         <h1 className="display-4">Welcome to My Landing Page</h1>
         <p className="lead">Learn more about my product or service and how it can benefit you.</p>
